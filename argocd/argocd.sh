2b2b13262c9663bee447b141f1f83e391c54bd3e
phuongdesktopUbuntu <ducphuong170498@gmail.com>
2023-06-18 03:59:28 +0700

    argo_eks

--- argocd/argocd.sh
+++ argocd/argocd.sh
@@ -3,4 +3,10 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl port-forward -n argocd service/argocd-server 8443:443
+# kubectl port-forward -n argocd service/argocd-server 8443:443
+
+kubectl patch svc -n argocd argocd-server --patch '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl  -n argocd get svc argocd-server
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
